98ece729062754ec02a2bf1e690279f5fe1f14c8
Mcrouse <matthew.crouse@temple.edu>
2021-04-11 20:07:25 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    require: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Must match an email address!",
+    ],
+  },
+
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+    ],
+
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+    
+
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
